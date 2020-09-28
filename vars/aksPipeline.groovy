e0d213bdd1ba7edbce904bf313a4a0e7e8297c33
Dipjoy Bhowmik <dipjoy.bhowmik@pwc.com>
2020-09-28 16:16:31 +0530

    Update akspipeline
    groovy script updated

--- vars/aksPipeline.groovy
+++ vars/aksPipeline.groovy
@@ -2,16 +2,13 @@ def call(){
     stage("Run Unit Test"){
         echo"Running Unit Test"
     }
-    stage("Docker Build"){   
+    stage 'Checkout'
+       git 'ssh://git@github.com:dipjoyb/testPipelineDipjoy.git'
 
-                    echo "[INFO]: Docker Build"
-
-                    script {
-                        sh 'docker build -t nodejs-jenkins-test:latest .'
-			sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 416764473985.dkr.ecr.us-east-1.amazonaws.com'
-			sh 'docker tag nodejs-jenkins-test:latest 416764473985.dkr.ecr.us-east-1.amazonaws.com/byjenkins:latest'
-			sh 'docker push 416764473985.dkr.ecr.us-east-1.amazonaws.com/byjenkins:latest'
-
-                    }                        
-    }
+    stage 'Docker build'
+       docker.build('byjenkins')
+    
+    stage 'Docker push'
+       docker.withRegistry('416764473985.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:bttrm-backend-user') {
+       docker.image('byjenkins').push('latest')
 }
